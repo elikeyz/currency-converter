6f1c1e5dd8f0d692ed560c8aa0af8773e4d751d7
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-02-05 00:47:25 +0100

    integrate the currency conversion functionality

--- main.js
+++ main.js
@@ -1,14 +1,55 @@
-const currFrom = document.getElementById('curr-from');
-const currTo = document.getElementById('curr-to');
+const currFromField = document.getElementById('curr-from');
+const currToField = document.getElementById('curr-to');
+const amountField = document.getElementById('amount');
+const resultDiv = document.getElementById('result');
+const submitBtn = document.getElementById('submit-btn');
+
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    fromCurrency = encodeURIComponent(fromCurrency);
+    toCurrency = encodeURIComponent(toCurrency);
+    const query = `${fromCurrency}_${toCurrency}`;
+
+    return fetch(`https://free.currencyconverterapi.com/api/v6/convert?q=${query}&compact=ultra`)
+        .then(response => response.json())
+        .then(data => {
+            const val = data[query];
+            const total = Math.round((val * amount * 100) / 100);
+            return total;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
 fetch('https://free.currencyconverterapi.com/api/v6/currencies')
     .then(response => response.json())
     .then(data => {
         const dataArr = Object.entries(data.results);
+        dataArr.sort();
         for (let i = 0; i < dataArr.length; i++) {
-            currFrom.insertAdjacentHTML('beforeend',
+            currFromField.insertAdjacentHTML('beforeend',
             `<option value="${dataArr[i][1].id}">${dataArr[i][1].currencyName} - ${dataArr[i][1].id}</option>`);
-            currTo.insertAdjacentHTML('beforeend',
+            currToField.insertAdjacentHTML('beforeend',
             `<option value="${dataArr[i][1].id}">${dataArr[i][1].currencyName} - ${dataArr[i][1].id}</option>`);
         }
     })
+    .catch(err => {
+        console.log(err);
+    });
+
+submitBtn.addEventListener('click', function(event) {
+    const amount = amountField.value;
+    const currFrom = currFromField.value;
+    const currTo = currToField.value;
+    amountField.value = '';
+
+    event.preventDefault();
+    convertCurrency(Number(amount), currFrom, currTo)
+        .then(result => {
+            resultDiv.innerHTML = `<p class="success">${amount} ${currFrom} equals ${result} ${currTo}</p>`
+        })
+});
+
+amountField.addEventListener('focus', function() {
+    resultDiv.innerHTML = '';
+});
