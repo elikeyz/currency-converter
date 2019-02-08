205f9492404db59ce01565c15893630c4c7802e6
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-02-08 23:35:13 +0100

    set up error handling and feedback and add icon

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const currFromField = document.getElementById('curr-from');
 const currToField = document.getElementById('curr-to');
 const amountField = document.getElementById('amount');
 const resultDiv = document.getElementById('result');
-const submitBtn = document.getElementById('submit-btn');
+const currencyForm = document.getElementsByTagName('form');
 
 function convertCurrency(amount, fromCurrency, toCurrency) {
     fromCurrency = encodeURIComponent(fromCurrency);
@@ -13,7 +13,7 @@ function convertCurrency(amount, fromCurrency, toCurrency) {
         .then(response => response.json())
         .then(data => {
             const val = data[query];
-            const total = Math.round((val * amount * 100) / 100);
+            const total = Math.round(val * amount * 100) / 100;
             return total;
         })
         .catch(err => {
@@ -34,19 +34,28 @@ fetch('https://free.currencyconverterapi.com/api/v6/currencies')
         }
     })
     .catch(err => {
-        console.log(err);
+        resultDiv.innerHTML = `<p class="error">An error was encountered while trying to get currency list. ${err}</p>`;
     });
 
-submitBtn.addEventListener('click', function(event) {
+currencyForm.addEventListener('submit', function(event) {
     const amount = amountField.value;
     const currFrom = currFromField.value;
     const currTo = currToField.value;
     amountField.value = '';
 
+    resultDiv.innerHTML = `<p class="working">Converting...</p>`;
+
     event.preventDefault();
     convertCurrency(Number(amount), currFrom, currTo)
         .then(result => {
-            resultDiv.innerHTML = `<p class="success">${amount} ${currFrom} equals ${result} ${currTo}</p>`
+            if(result !== undefined) {
+                resultDiv.innerHTML = `<p class="success">${amount} ${currFrom} equals ${result} ${currTo}</p>`;
+            } else {
+                resultDiv.innerHTML = `<p class="error">Failed to get conversion rate</p>`;
+            }
+        })
+        .catch(err => {
+            resultDiv.innerHTML = `<p class="error">An error was encountered while trying to convert currencies. ${err}</p>`;
         })
 });
 
