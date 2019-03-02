3f4f83b38ca80f7196c55f785265dd09da4449a9
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2019-03-02 14:13:32 +0100

    change the orientation property in manifest.json

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'currency-converter-static-v2';
+const staticCacheName = 'currency-converter-static-v3';
 const conversionsCache = 'currency-converter-conversions';
 const allCaches = [staticCacheName, conversionsCache];
 
